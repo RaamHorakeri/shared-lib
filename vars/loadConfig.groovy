5b4b1fbd8d5382c5c42728e9ad0153ee80f4f444
Raam <123749532+RaamHorakeri@users.noreply.github.com>
2025-04-04 11:55:07 +0530

    Update loadConfig.groovy

--- vars/loadConfig.groovy
+++ vars/loadConfig.groovy
@@ -52,6 +52,23 @@ def call() {
                     ]
                 ]
             ]
+            webcalc: [
+                environments: [
+                    dev: [
+                        agentName: '',
+                        repoUrl: 'https://github.com/RaamHorakeri/simple-calculator.git',
+                        branch: 'main',
+                        credentialsId: 'gittoken',
+                        envVars: [
+                            SENDGRID_KEY: 'SENDGRID_KEY_DEV',
+                            MONGO_CONNECTION_STRING: 'MONGO_CONNECTION_STRING_DEV',
+                            BIFROST_ACCOUNT_PROFILE_API: 'BIFROST_ACCOUNT_PROFILE_API_DEV',
+                            ODIN_SECRET: 'ODIN_SECRET_DEV',
+                            ODIN_HOST: 'ODIN_HOST_DEV'
+                        ]
+                    ]
+                ]
+            ]
         ]
     ]
 
